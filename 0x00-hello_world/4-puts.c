5e9720f6ebb074475ceab12d60a6152cc34d3a04
Your Name <you@example.com>
2023-02-17 06:41:46 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function without using printf
- * Return: 0 (success)
+ * main - A program that prints a line with puts function without using print command
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-Return (0);
-}
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
