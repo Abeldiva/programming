520c57fc4a45c5172335a826b7579f49ee52d267
Your Name <you@example.com>
2023-02-20 10:08:21 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,8 @@
 
 /**
  * main - Entry point
- *
+ * Description: prints if a number is positive or begative
+ * betty style doc for function main goes there
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -12,7 +13,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - Rand_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
