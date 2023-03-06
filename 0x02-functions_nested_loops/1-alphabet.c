732f08f21381285d372720c4cad778554d592584
Your Name <you@example.com>
2023-03-06 15:30:08 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "main.h"
+
 /**
- * main - Entry point
- * Description: print all alphabet in lowercase
- * Return: void
+ * print_alphabet - print all alphabet in lowercase
  */
-void print_alphabet(void);
+
+void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-	{
 		_putchar(letter);
-	}
+
 	_putchar('\n');
 }
